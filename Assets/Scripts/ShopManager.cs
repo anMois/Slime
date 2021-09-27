4e31d048ebd348cfbcb8f52ba3a328f29fe301ca
anMois <66979888+anMois@users.noreply.github.com>
2021-09-27 17:17:34 +0900

    0927

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -18,10 +18,19 @@ public class ShopManager : MonoBehaviour
         _uiManager = GameObject.Find("OBJ").GetComponent<UIManager>();
     }
 
+    private void Start()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        gameObject.transform.GetChild(1).gameObject.SetActive(false);
+    }
+
     private void Update()
     {
         if(slimes_count.slim.cleaner_s >= 2)
         {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+
             shCoin_num += Time.deltaTime;
 
             if (shCoin_num > 10.0f)
