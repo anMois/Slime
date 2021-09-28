1857d825ae3e02bac8075fca998458ea20cf1c45
anMois <66979888+anMois@users.noreply.github.com>
2021-09-28 17:15:14 +0900

    0928

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -18,19 +18,10 @@ public class ShopManager : MonoBehaviour
         _uiManager = GameObject.Find("OBJ").GetComponent<UIManager>();
     }
 
-    private void Start()
-    {
-        gameObject.transform.GetChild(0).gameObject.SetActive(true);
-        gameObject.transform.GetChild(1).gameObject.SetActive(false);
-    }
-
     private void Update()
     {
         if(slimes_count.slim.cleaner_s >= 2)
         {
-            gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            gameObject.transform.GetChild(1).gameObject.SetActive(true);
-
             shCoin_num += Time.deltaTime;
 
             if (shCoin_num > 10.0f)
@@ -39,6 +30,14 @@ public class ShopManager : MonoBehaviour
             }
         }
         
+    }
+
+    public void CheckSlime()
+    {
+        if (slimes_count.slim.cleaner_s >= 2)
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        else
+            Debug.Log("클리너 슬라임이 부족합니다.");
     }
 
     public void AddCoin()
