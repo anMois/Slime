eba1550fce5ce68515f8b96bbcd9ece8c9233cfc
anMois <66979888+anMois@users.noreply.github.com>
2022-06-09 15:55:27 +0900

    22.0609

--- Assets/Scripts/SlimeAutoMove.cs
+++ Assets/Scripts/SlimeAutoMove.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class SlimeAutoMove : MonoBehaviour
 {
-    public float num;
+    public float idle_Num;
 
     float speedX;
     float speedY;
-    float num1 = 2.5f;
+    float walk_Num = 2.5f;
 
     bool isidle = true;
     bool iswalk = false;
+
+    public GameObject TopLeft;
+    public GameObject BottomRight;
+
+    Transform tl;
+    Transform br;
 
     Animator isMove;
     SpriteRenderer slime_sprite;
@@ -20,27 +26,33 @@ public class SlimeAutoMove : MonoBehaviour
     {
         isMove = transform.GetComponent<Animator>();
         slime_sprite = transform.GetComponent<SpriteRenderer>();
+        tl = TopLeft.transform;
+        br = BottomRight.transform;
     }
 
     private void Start()
     {
-        NewNum();
+        Newidle_Num();
     }
 
     private void Update()
     {
+        //idle
         if (isidle == true && iswalk == false)
         {
-            num -= Time.deltaTime;
-            if (num < 0.0f)
+            idle_Num -= Time.deltaTime;
+            if (idle_Num < 0.0f)
             {
                 isMoveCheck();
             }
         }
+        //walk
         else
         {
-            num1 -= Time.deltaTime;
-            if (num1 < 0.0f)
+            transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedY * Time.deltaTime);
+            SelfPositionCheck();
+            walk_Num -= Time.deltaTime;
+            if (walk_Num < 0.0f)
             {
                 isMoveCheck();
             }
@@ -48,33 +60,37 @@ public class SlimeAutoMove : MonoBehaviour
         
     }
 
-
-    float NewNum()
+    float Newidle_Num()
     {
-        num = Random.Range(3.0f, 5.0f);
-        Debug.Log(num);
-        return num;
+        idle_Num = Random.Range(3.0f, 5.0f);
+        return idle_Num;
     }
 
     void isMoveCheck()
     {
+        //idle
         if (isMove.GetBool("isWalk"))
         {
             isMove.SetBool("isWalk", false);
             isidle = true;
             iswalk = false;
-            num = NewNum();
+            idle_Num = Newidle_Num();
         }
+        //walk
         else
         {
+            
+            walk_Num = 2.5f;
             isMove.SetBool("isWalk", true);
-            transform.Translate(RandomSpeedX(), RandomSpeedY(), RandomSpeedY());
+            speedX = RandomSpeedX();
+            speedY = RandomSpeedY();
             isidle = false;
             iswalk = true;
         }
         
     }
 
+    #region RandomSpeed
     float RandomSpeedX()
     {
         speedX = Random.Range(-0.8f, 0.8f);
@@ -91,4 +107,19 @@ public class SlimeAutoMove : MonoBehaviour
         speedY = Random.Range(-0.8f, 0.8f);
         return speedY;
     }
+    #endregion
+
+    #region 자기위치 확인
+    void SelfPositionCheck()
+    {
+        if (transform.position.x < tl.position.x || transform.position.x > br.position.x)
+        {
+            Debug.Log("Outsite");
+        }
+        else if (transform.position.y > tl.position.y || transform.position.y < br.position.y)
+        {
+            Debug.Log("Outsite");
+        }
+    }
+    #endregion
 }
