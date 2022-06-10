62adc6ffdb7e78374efc825af4d026f46ca14012
anMois <66979888+anMois@users.noreply.github.com>
2022-06-10 15:32:53 +0900

    22.0610

--- Assets/Scripts/SlimeAutoMove.cs
+++ Assets/Scripts/SlimeAutoMove.cs
@@ -8,16 +8,18 @@ public class SlimeAutoMove : MonoBehaviour
 
     float speedX;
     float speedY;
-    float walk_Num = 2.5f;
+    float walk_Num;
 
     bool isidle = true;
     bool iswalk = false;
 
     public GameObject TopLeft;
     public GameObject BottomRight;
+    public GameObject Manager;
 
-    Transform tl;
-    Transform br;
+    Transform tl; //topleft 위치
+    Transform br; //bottomright 위치
+    Vector3 point; //돌아갈 위치
 
     Animator isMove;
     SpriteRenderer slime_sprite;
@@ -32,94 +34,85 @@ public class SlimeAutoMove : MonoBehaviour
 
     private void Start()
     {
-        Newidle_Num();
+        idle_Num = Random.Range(3.0f, 5.0f);
     }
 
     private void Update()
     {
-        //idle
+        //idle 상태
         if (isidle == true && iswalk == false)
         {
             idle_Num -= Time.deltaTime;
             if (idle_Num < 0.0f)
             {
-                isMoveCheck();
+                isWalkCheck();
             }
         }
-        //walk
+        //walk 상태
         else
         {
             transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedY * Time.deltaTime);
-            SelfPositionCheck();
+            if (SelfPositionCheck())
+            {
+                transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedY * Time.deltaTime);
+            }
             walk_Num -= Time.deltaTime;
             if (walk_Num < 0.0f)
             {
-                isMoveCheck();
+                isWalkCheck();
             }
         }
-        
     }
 
-    float Newidle_Num()
+    void isWalkCheck()
     {
-        idle_Num = Random.Range(3.0f, 5.0f);
-        return idle_Num;
-    }
-
-    void isMoveCheck()
-    {
-        //idle
+        //idle 전환
         if (isMove.GetBool("isWalk"))
         {
             isMove.SetBool("isWalk", false);
-            isidle = true;
-            iswalk = false;
-            idle_Num = Newidle_Num();
+            idle_Num = Random.Range(3.0f, 5.0f);
+            isidle = true;  iswalk = false;
         }
-        //walk
+        //walk 전환
         else
         {
-            
             walk_Num = 2.5f;
             isMove.SetBool("isWalk", true);
-            speedX = RandomSpeedX();
-            speedY = RandomSpeedY();
-            isidle = false;
-            iswalk = true;
+            speedX = Random.Range(-0.8f, 0.8f);
+            speedY = Random.Range(-0.8f, 0.8f);
+
+            if (speedX < 0)
+                slime_sprite.flipX = true;
+            else
+                slime_sprite.flipX = false;
+
+            isidle = false; iswalk = true;
         }
-        
     }
-
-    #region RandomSpeed
-    float RandomSpeedX()
-    {
-        speedX = Random.Range(-0.8f, 0.8f);
-
-        if (speedX < 0)
-            slime_sprite.flipX = true;
-        else
-            slime_sprite.flipX = false;
-
-        return speedX;
-    }
-    float RandomSpeedY()
-    {
-        speedY = Random.Range(-0.8f, 0.8f);
-        return speedY;
-    }
-    #endregion
 
     #region 자기위치 확인
-    void SelfPositionCheck()
+    bool SelfPositionCheck()
     {
-        if (transform.position.x < tl.position.x || transform.position.x > br.position.x)
+        if ((transform.position.x < tl.position.x || transform.position.x > br.position.x) ||
+            (transform.position.y > tl.position.y || transform.position.y < br.position.y))
         {
-            Debug.Log("Outsite");
+            int n = Random.Range(0, 3); //지정된 위치 정하기
+            point = Manager.GetComponent<GameManager>().PointList[n];
+            Vector3 v = point - transform.position;
+            v.Normalize();
+            v = v * 0.3f;
+            speedX = v.x;
+            speedY = v.y;
+
+            if (speedX < 0)
+                slime_sprite.flipX = true;
+            else
+                slime_sprite.flipX = false;
+
+            return true;
         }
-        else if (transform.position.y > tl.position.y || transform.position.y < br.position.y)
-        {
-            Debug.Log("Outsite");
-        }
+        else
+            return false;
     }
     #endregion
 }
