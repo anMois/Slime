1335a5e42240915f4ec2628ed4ab11bdafe9a5fb
anMois <66979888+anMois@users.noreply.github.com>
2022-06-20 19:01:19 +0900

    22.0620

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Vector3[] PointList; 
+    public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void Change(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,24 +10,44 @@ public class UIManager : MonoBehaviour
     public int SlimeCount;
     public int MaxSlime;
 
-    int value;
-    
-    Slime _slime;
-
     public Text gold_Text;
     public Text slimecount_Text;
+
+    public Sprite showsp;
+    public Sprite hidesp;
+
+    int value;
+
+    public bool isClick;
+
+    GameObject option_Panel;
+
+    Image plant_Img;
+    Animator _ani;
+
+    Slime _slime;  
 
     private void Awake()
     {
         gold_Text = GameObject.Find("Gold/Text").GetComponent<Text>();
         slimecount_Text = GameObject.Find("SlimeCount/Text").GetComponent<Text>();
+        plant_Img = GameObject.Find("LeftBtn/Plant Button").GetComponent<Image>();
+
+        option_Panel = GameObject.Find("Option Panel");
+        _ani = GameObject.Find("Slime Panel").GetComponent<Animator>();
 
         _slime = GameObject.Find("GameManager").GetComponent<Create_Slime>()._slime;
     }
 
     private void Start()
     {
+        option_Panel.SetActive(false);
         MaxSlime = 10;
+    }
+
+    private void Update()
+    {
+        Option();
     }
 
     private void LateUpdate()
@@ -49,6 +69,41 @@ public class UIManager : MonoBehaviour
         {
             Gold -= 500;
             MaxSlime += 5;
+        }
+    }
+
+    public void ShowPanel()
+    {
+        if (isClick)
+        {
+            _ani.SetTrigger("doHide");
+            plant_Img.sprite = hidesp;
+            isClick = false;
+        }
+        else
+        {
+            _ani.SetTrigger("doShow");
+            plant_Img.sprite = showsp;
+            isClick = true;
+        }
+    }
+
+    public void Option()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isClick)
+            {
+                option_Panel.SetActive(false);
+                Time.timeScale = 1;
+                isClick = false;
+            }
+            else
+            {
+                option_Panel.SetActive(true);
+                Time.timeScale = 0;
+                isClick = true;
+            }
         }
     }
 }
